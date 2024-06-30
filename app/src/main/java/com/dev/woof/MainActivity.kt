4b80ccdf5e44700331082ab2dab219e6b06c6d94
Vandal <86826719+VandalByte@users.noreply.github.com>
2024-06-30 08:05:37 +0530

    Committing local changes before revert

--- app/src/main/java/com/dev/woof/MainActivity.kt
+++ app/src/main/java/com/dev/woof/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import java.security.SecureRandom
+import android.util.Base64
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val nonce = generateNonce() // Generate a 16-byte nonce
+
+        val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(true)
+            .setServerClientId(getString(R.string.web_client_id))
+            .setAutoSelectEnabled(true)
+            .setNonce(nonce)
+            .build()
+    }
+
+    private fun generateNonce(): String {
+        val length = 16
+        val random = SecureRandom()
+        val nonceBytes = ByteArray(length)
+        random.nextBytes(nonceBytes)
+        return Base64.encodeToString(nonceBytes, Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP)
     }
 }
