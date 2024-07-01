640d3a978d7cb7b219a4e939823d597fbee67ad2
Niranjan Suresh <131999284+Major616@users.noreply.github.com>
2024-07-01 22:05:41 +0530

    splash screen

---
+++ app/src/main/java/com/dev/woof/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.dev.woof
+
+// src/main/java/com/yourpackage/SplashActivity.kt
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 5000 // 5 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            // Start your main activity
+            startActivity(Intent(this, MainActivity::class.java))
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
