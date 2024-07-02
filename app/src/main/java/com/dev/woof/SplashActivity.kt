283b7d3ccd8244bdf295cedaf53fcdc079525752
Niranjan Suresh <131999284+Major616@users.noreply.github.com>
2024-07-02 11:44:32 +0530

    splash screen animation

--- app/src/main/java/com/dev/woof/SplashActivity.kt
+++ app/src/main/java/com/dev/woof/SplashActivity.kt
@@ -1,26 +1,29 @@
 package com.dev.woof
 
-// src/main/java/com/yourpackage/SplashActivity.kt
 
 
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
 
-    private val SPLASH_TIME_OUT: Long = 5000 // 5 seconds
-
+    private val SPLASH_TIME_OUT: Long = 4000 // 4 sec
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val splashLogo: ImageView = findViewById(R.id.splash_logo)
+        val fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        splashLogo.startAnimation(fadeIn)
+
         Handler().postDelayed({
-            // Start your main activity
             startActivity(Intent(this, MainActivity::class.java))
-            // close this activity
             finish()
         }, SPLASH_TIME_OUT)
     }
 }
+
