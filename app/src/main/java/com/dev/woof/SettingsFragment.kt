248ef81de8fb81d497436fe567006a2eb3376f2f
lunar-alt <suryakiran.andoor2022@vitstudent.ac.in>
2024-07-09 22:37:03 +0530

    Linear Layout added

--- app/src/main/java/com/dev/woof/SettingsFragment.kt
+++ app/src/main/java/com/dev/woof/SettingsFragment.kt
@@ -1,40 +1,4 @@
-/*
-package com.dev.woof
 
-import android.content.Context
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.EditText
-import androidx.fragment.app.Fragment
-
-class SettingsFragment : Fragment(R.layout.fragment_settings) {
-
-    private lateinit var nameEditText: EditText
-    private lateinit var emailEditText: EditText
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // Initialize the EditText fields
-        nameEditText = view.findViewById(R.id.name)
-        emailEditText = view.findViewById(R.id.email)
-
-        // Load user data from SharedPreferences
-        loadUserData()
-    }
-
-    private fun loadUserData() {
-        val sharedPref = activity?.getSharedPreferences("user_data", Context.MODE_PRIVATE)
-        val userName = sharedPref?.getString("user_name", "")
-        val userEmail = sharedPref?.getString("user_email", "")
-
-        nameEditText.setText(userName)
-        emailEditText.setText(userEmail)
-    }
-}
-*/
 package com.dev.woof
 
 
@@ -53,6 +17,13 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
     private lateinit var emailEditText: EditText
     private lateinit var profileImageView: ImageView
 
+    // Listener interface for profile photo changes
+    interface OnProfilePhotoChangedListener {
+        fun onProfilePhotoChanged(photoUrl: String?)
+    }
+
+    private var profilePhotoChangedListener: OnProfilePhotoChangedListener? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -65,6 +36,10 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
         loadUserData()
         // Load profile image from SharedPreferences
         loadProfileImage()
+    }
+    // Method to set the profile photo changed listener
+    fun setOnProfilePhotoChangedListener(listener: OnProfilePhotoChangedListener) {
+        profilePhotoChangedListener = listener
     }
 
     private fun loadUserData() {
@@ -80,12 +55,14 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
         val sharedPref = requireActivity().getSharedPreferences("user_data", Context.MODE_PRIVATE)
         val profilePhotoUrl = sharedPref.getString("profile_photo_url", "")
 
-        // Load profile image using Glide (or Picasso)
+        // Load profile image using Glide
         Glide.with(requireContext())
             .load(profilePhotoUrl)
             .placeholder(R.drawable.img)
             .error(R.drawable.img)
             .circleCrop()
             .into(profileImageView)
+        // Notify listener about profile photo change
+        profilePhotoChangedListener?.onProfilePhotoChanged(profilePhotoUrl)
     }
 }
