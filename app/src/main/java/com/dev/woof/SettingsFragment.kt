6ac8429f27e61a155282168ce3075a5d8cc05620
Vandal <86826719+VandalByte@users.noreply.github.com>
2024-07-09 22:42:28 +0530

    Removed unused code

--- app/src/main/java/com/dev/woof/SettingsFragment.kt
+++ app/src/main/java/com/dev/woof/SettingsFragment.kt
@@ -1,46 +1,6 @@
-/*
 package com.dev.woof
 
 import android.content.Context
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.EditText
-import androidx.fragment.app.Fragment
-
-class SettingsFragment : Fragment(R.layout.fragment_settings) {
-
-    private lateinit var nameEditText: EditText
-    private lateinit var emailEditText: EditText
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // Initialize the EditText fields
-        nameEditText = view.findViewById(R.id.name)
-        emailEditText = view.findViewById(R.id.email)
-
-        // Load user data from SharedPreferences
-        loadUserData()
-    }
-
-    private fun loadUserData() {
-        val sharedPref = activity?.getSharedPreferences("user_data", Context.MODE_PRIVATE)
-        val userName = sharedPref?.getString("user_name", "")
-        val userEmail = sharedPref?.getString("user_email", "")
-
-        nameEditText.setText(userName)
-        emailEditText.setText(userEmail)
-    }
-}
-*/
-package com.dev.woof
-
-
-
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
